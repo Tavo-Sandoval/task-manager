0da29a9a73153f71ae6ae3e0b7e444588fec1e1a
Tavo-Sandoval <tavo28.san@gmail.com>
2025-04-30 09:48:06 -0600

    otro cambio

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,5 @@ module.exports = {
   // ...existing code...
   rules: {
     // ...existing rules...
-    '@typescript-eslint/no-unused-expressions': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
