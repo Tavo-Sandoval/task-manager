dd77bce3d88b68c8fb1165f04e645cc7d19fc692
Tavo-Sandoval <tavo28.san@gmail.com>
2025-04-30 09:59:15 -0600

    r

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   // ...existing code...
+  plugins: ["@typescript-eslint"], // Asegúrate de incluir el plugin
   rules: {
     // ...existing rules...
     "@typescript-eslint/no-unused-expressions": "off", // Desactiva la regla
